e46d154cd28d44ffd9faaf6a86dbadff037db491
Leon Gabriel <g73346920@gmail.com>
2024-09-01 22:58:13 +0300

    update:the media screen width when being adjusted or re-adjusted

--- src/dashboard/Layout.tsx
+++ src/dashboard/Layout.tsx
@@ -3,7 +3,12 @@ export default function dashboardLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (< div className="" >
+  return (< div className="h-screen flex" >
+    {/*Left*/}
+     <div className="w-[14%] md:[] bg-red-200">l</div>
+     {/*Right*/}
+     <div className="w-[86%] bg-blue-20">r</div>
+
     
   </div>
 
