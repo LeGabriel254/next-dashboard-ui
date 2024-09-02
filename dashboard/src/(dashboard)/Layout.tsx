7df368f963667ff476190c97c716be9fd51c2b7d
Leon Gabriel <g73346920@gmail.com>
2024-09-02 12:31:48 +0300

    modified changes made

--- dashboard/src/(dashboard)/Layout.tsx
+++ dashboard/src/(dashboard)/Layout.tsx
@@ -7,16 +7,16 @@ export default function DashboardLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return < div className="h-screen flex" >
-    {/*Left*/}
-     <div className='w--[14%] md:w[8%] lg:w--[16%] xl:[14%] bg-red-200'>
+  return  < div className="h-screen flex" >
+  {/*Left*/}
+   <div className='w--[14%] md:w[8%] lg:w--[16%] xl:[14%] bg-red-200'>
 <Link href="/" className="flex items-center justify-center gap-2">
-  {/* <image src='/public/logo.png' alt="logo" width={32} height={32} /> */}
-  <span className="hidden lg:block">Schooleon</span>
+<img src='/public/image/logo.png' alt="logo" width={32} height={32} /> 
+<span className="hidden lg:block">Schooleon</span>
 </Link>
-     </div>
-     {/*Right*/}
-     <div className='w--[86%] md:[92%] lg:w--[84%] xl:[86%] bg-blue-20'>r</div>  
-  </div>
+   </div>
+   {/*Right*/}
+   <div className='w--[86%] md:[92%] lg:w--[84%] xl:[86%] bg-blue-20'>r</div>  
+</div>
     }
   
