534fc657a96e7adf48088343959f0f9e28aa2fee
Leon Gabriel <g73346920@gmail.com>
2024-09-04 18:18:27 +0300

    Menu

--- dashboard/src/components/Menu.tsx
+++ dashboard/src/components/Menu.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const menuItems = [
   {
     title: "MENU",
@@ -113,13 +115,32 @@ const menuItems = [
   },
 ];
 
-const menu =() =>{
-  //  return(
-  //    <div className="" key={.title}>
-  // /   <span>{i.title}</span>
-
-  //     </div>
-  // )
-        
-      }
-  
+//  export const Menu = () => {
+//   return (
+//     <div className="mt-4 text-sm">
+//       {menuItems.map((i) => (
+//         <div className="flex flex-col gap-2" key={i.title}>
+//           <span className="hidden lg:block text-gray-400 font-light my-4">
+//             {i.title}
+//           </span>
+//           {i.items.map((item) => {
+//             if (item.visible.includes(role)) {
+//               return (
+//                 <>
+//                 <Link
+//                   href={item.href}
+//                   key={item.label}
+//                   className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight"
+//                 >
+//                   <img src={item.icon} alt="" width={20} height={20} />
+//                   <span className="hidden lg:block">{item.label}</span>
+//                 </Link>
+//                 </>
+//               );
+//             }
+//           })}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
