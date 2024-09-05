7f05db6d62418bb217521937a3055a0f919fda74
Leon Gabriel <g73346920@gmail.com>
2024-09-05 12:41:41 +0300

    Modified

--- dashboard/src/(dashboard)/Layout.tsx
+++ dashboard/src/(dashboard)/Layout.tsx
@@ -1,7 +1,7 @@
-// import Menu from "@/components/Menu";
+
 // import Navbar from "@/components/Navbar";
-import Image from "next/image";
 import Link from "next/link";
+import Menu from "../components/Menu";
 
 export default function DashboardLayout({
   children,
@@ -11,19 +11,20 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] ">
         <Link
           href="/"
-          className="flex items-center justify-center lg:justify-start gap-2"
+          className="flex items-center justify-center lg:justify-start gap-2 p-4"
         >
-          <Image src="/logo.png" alt="logo" width={32} height={32} />
+           {/* <img src={logo.jpg} alt='logo' />  */}
           <span className="hidden lg:block font-bold">SchooLama</span>
         </Link>
-        {/* <Menu /> */}
+        {/* <Menu />  */}
       </div>
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] bg-blue-200 overflow-scroll flex flex-col">
         <Navbar />
+        <Menu />
         {children}
       </div>
     </div>
